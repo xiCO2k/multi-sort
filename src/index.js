59961b604636dc70c7edb0d9b91cf2e1e5774519
Francisco Madeira <xico2k@gmail.com>
2017-10-07 21:33:31 +0100

    add all tests

--- src/index.js
+++ src/index.js
@@ -1,91 +1,108 @@
-import _ from 'lodash';
-
 function validateData(data) {
-    let throwMessage = 'It can\'t sort, validate the data sent';
-    if (!data ||
-        typeof data === "number" ||
-        typeof data === "boolean") {
-        throw new Error(throwMessage);
+    if (!data || typeof data !== 'object' ||
+        Array.isArray(data) && data.length &&
+        (Array.isArray(data[0]) || typeof data[0] !== 'object')) {
+        throw new Error('It can\'t sort, validate the data sent');
     }
-
-
 }
 
 function validateKey(key) {
     if (typeof key !== 'string') {
-        throw new Error("The key param needs to be a string");
+        throw new Error('The key param needs to be a string');
     }
 }
 
-function validateParams(data, columns, order_by, key) {
+function validateParams(data, columns, orderBy, key) {
     validateData(data);
     validateKey(key);
 }
 
-export default function MultiSort(arr = [], columns = null, order_by = null, key = "_key") {
-    validateParams(arr, columns, order_by, key); //it throws
+function formatArguments(data = [], args) {
+    let columns = [],
+        orderBy = [],
+        key = '_key';
 
-    if (!Array.isArray(arr)) {
-        let data = [];
+    //If is Object
+    if (args[0] !== null &&
+        typeof args[0] === 'object' &&
+        !Array.isArray(args[0])) {
 
-        for (let i in arr) {
-            data.push({
-                ...arr[i],
-                [key]: i
-            });
+        for (let column in args[0]) {
+            columns.push(column);
+            orderBy.push(args[0][column]);
         }
 
-        arr = data;
+        key = args[1] || key;
+    } else {
+        columns = args[0];
+        orderBy = args[1];
+
+        if (typeof columns === 'string') {
+            columns = [columns];
+        }
+
+        if (typeof orderBy === 'string') {
+            orderBy = [orderBy];
+        }
+
+        key = args[2] || key;
     }
 
-    if (!arr.length) {
-        return [];
+    return { columns, orderBy, key };
+}
+
+function formatData(arr, key) {
+    if (Array.isArray(arr)) {
+        return arr;
     }
 
-    if (Array.isArray(arr[0])) {
-        throw new Error("It can't sort, validate the data sent");
+    let data = [];
+
+    for (let i in arr) {
+        data.push({ ...arr[i], [key]: i });
     }
 
-    if (typeof columns === "string") {
-        columns = [columns];
+    return data;
+}
+
+function sort(a, b, columns, orderBy, index) {
+    let direction = orderBy[index] == 'DESC' ? 1 : 0;
+
+    if (typeof a[columns[index]] === 'boolean' ||
+        typeof a[columns[index]] === 'undefined') {
+        a[columns[index]] = a[columns[index]] ? 1 : 0;
     }
 
-    if (typeof order_by === "string") {
-        order_by = [order_by];
+    if (typeof b[columns[index]] === 'boolean' ||
+        typeof b[columns[index]] === 'undefined') {
+        b[columns[index]] = b[columns[index]] ? 1 : 0;
     }
 
-    function multisort_recursive(a, b, columns, order_by, index) {
-        let direction = order_by[index] == 'DESC' ? 1 : 0;
-
-        if( _.isBoolean( a[columns[index]] ) || _.isUndefined( a[columns[index]] ) ) a[columns[index]] = a[columns[index]] ? 1 : 0;
-        if( _.isBoolean( b[columns[index]] ) || _.isUndefined( b[columns[index]] ) ) b[columns[index]] = b[columns[index]] ? 1 : 0;
-
-        var is_numeric = !isNaN(+a[columns[index]] - +b[columns[index]]);
-
-        var x = is_numeric ?
+    let isNumeric = !isNaN(+a[columns[index]] - +b[columns[index]]),
+        x = isNumeric ?
             +a[columns[index]] :
-            _.isArray(a[columns[index]]) ?
+            Array.isArray(a[columns[index]]) ?
                 a[columns[index]] :
-                ('' + a[columns[index]]).toLowerCase();
-
-        var y = is_numeric ?
+                ('' + a[columns[index]]).toLowerCase(),
+        y = isNumeric ?
             +b[columns[index]] :
-            _.isArray(b[columns[index]]) ?
+            Array.isArray(b[columns[index]]) ?
                 b[columns[index]] :
                 ('' + b[columns[index]]).toLowerCase();
 
-        if(x < y) {
-            return direction == 0 ? -1 : 1;
-        }
-
-        if(x == y)  {
-            return columns.length-1 > index ? multisort_recursive(a,b,columns,order_by,index+1) : 0;
-        }
-
-        return direction == 0 ? 1 : -1;
+    if (x < y) {
+        return direction == 0 ? -1 : 1;
     }
 
-    return arr.sort(function (a,b) {
-        return multisort_recursive(a,b,columns,order_by,0);
-    });
+    if (x == y)  {
+        return columns.length - 1 > index ? sort(a, b, columns, orderBy, index+1) : 0;
+    }
+
+    return direction == 0 ? 1 : -1;
+}
+
+export default function MultiSort(arr = [], ...args) {
+    let { columns, orderBy, key } = formatArguments(arr, args);
+    validateParams(arr, columns, orderBy, key); //it throws
+    return formatData(arr, key).sort((a, b) => sort(a, b, columns, orderBy, 0));
 }
--- src/index.test.js
+++ src/index.test.js
@@ -2,35 +2,38 @@ import MultiSort from './index';
 
 it('can sort an array with objects', () => {
     let data = [
-        { first_name: "John", last_name: "Doe" },
-        { first_name: "Anthony", last_name: "Zipher" },
-        { first_name: "Anthony", last_name: "Carlos" }
+        { first_name: 'John', last_name: 'Doe' },
+        { first_name: 'Anthony', last_name: 'Zipher' },
+        { first_name: 'Anthony', last_name: 'Carlos' }
     ];
 
-    expect(MultiSort(data, "first_name", "ASC")[0].first_name).toBe("Anthony");
-    expect(MultiSort(data, ["first_name", "last_name"], ["ASC", "ASC"])[0].last_name).toBe("Carlos");
-    expect(MultiSort(data, ["first_name", "last_name"], ["ASC", "DESC"])[0].last_name).toBe("Zipher");
+    expect(MultiSort(data, 'first_name', 'ASC', '_key')[0].first_name).toBe('Anthony');
+    expect(MultiSort(data, ['first_name', 'last_name'], ['ASC', 'ASC'])[0].last_name).toBe('Carlos');
+    expect(MultiSort(data, ['first_name', 'last_name'], ['ASC', 'DESC'])[0].last_name).toBe('Zipher');
+    expect(MultiSort(data, { first_name: 'ASC', last_name: 'DESC' })[0].last_name).toBe('Zipher');
 });
 
 it('can sort an object with objects', () => {
     let data = {
-        key1: { first_name: "John", last_name: "Doe" },
-        key2: { first_name: "Anthony", last_name: "Zipher" },
-        key3: { first_name: "Anthony", last_name: "Carlos" }
+        key1: { first_name: 'John', last_name: 'Doe' },
+        key2: { first_name: 'Anthony', last_name: 'Zipher' },
+        key3: { first_name: 'Anthony', last_name: 'Carlos' }
     };
 
-    expect(MultiSort(data, ["first_name"], ["ASC"])[0].first_name).toBe("Anthony");
-    expect(MultiSort(data, ["first_name", "last_name"], ["ASC", "ASC"])[0].last_name).toBe("Carlos");
-    expect(MultiSort(data, ["first_name", "last_name"], ["ASC", "DESC"])[0].last_name).toBe("Zipher");
+    expect(MultiSort(data, 'first_name', 'ASC')[0].first_name).toBe('Anthony');
+    expect(MultiSort(data, ['first_name', 'last_name'], ['ASC', 'ASC'])[0].last_name).toBe('Carlos');
+    expect(MultiSort(data, ['first_name', 'last_name'], ['ASC', 'DESC'])[0].last_name).toBe('Zipher');
 });
 
 it('must have the object key inside of the object', () => {
     let data = {
-        key1: { first_name: "John", last_name: "Doe" }
+        key1: { first_name: 'John', last_name: 'Doe' }
     };
 
-    expect(MultiSort(data, ["first_name"], ["ASC"])[0]._key).toBe("key1");
-    expect(MultiSort(data, ["first_name"], ["ASC"], "customKeyName")[0].customKeyName).toBe("key1");
+    expect(MultiSort(data, ['first_name'], ['ASC'])[0]._key).toBe('key1');
+    expect(MultiSort(data, ['first_name'], ['ASC'], 'customKeyName')[0].customKeyName).toBe('key1');
+    expect(MultiSort(data, 'first_name', 'ASC', 'customKeyName')[0].customKeyName).toBe('key1');
+    expect(MultiSort(data, { 'first_name': 'ASC' }, 'customKeyName')[0].customKeyName).toBe('key1');
 });
 
 it('can handle empty data', () => {
@@ -39,34 +42,18 @@ it('can handle empty data', () => {
 });
 
 it('throws if is not multidimensional', () => {
-    expect(() => MultiSort({ first_name: "John", last_name: "Doe" })).toThrow();
+    expect(() => MultiSort({ first_name: 'John', last_name: 'Doe' })).toThrow();
     expect(() => MultiSort(false)).toThrow();
     expect(() => MultiSort(null)).toThrow();
     expect(() => MultiSort([[1, 2, 3, 4]])).toThrow();
     expect(() => MultiSort([[]])).toThrow();
     expect(() => MultiSort([[[1,2,3,4]]])).toThrow();
-    expect(() => MultiSort("string")).toThrow();
+    expect(() => MultiSort('string')).toThrow();
     expect(() => MultiSort(1234)).toThrow();
     expect(() => MultiSort(true)).toThrow();
 });
 
-// it('throws if "columns" parameter are not array or string', () => {
-//     let data = [{ first_name: "John", last_name: "Doe" }];
-
-//     expect(() => MultiSort(data, 1234)).toThrow();
-//     expect(() => MultiSort(data, {})).toThrow();
-//     expect(() => MultiSort(data, [[]])).toThrow();
-//     expect(() => MultiSort(data, [{}])).toThrow();
-// });
-
-// it('throws if "orderBy" parameter are not array or strings', () => {
-//     expect(() => MultiSort(data, null, 1234)).toThrow();
-//     expect(() => MultiSort(data, null, {})).toThrow();
-//     expect(() => MultiSort(data, null, [[]])).toThrow();
-// });
-
-// it('throws if "key" parameter are not strings', () => {
-//     expect(() => MultiSort(data, null, null, 1234)).toThrow();
-//     expect(() => MultiSort(data, null, null, {})).toThrow();
-//     expect(() => MultiSort(data, null, null, [])).toThrow();
-// });
+it('throws if the key is not string', () => {
+    expect(() => MultiSort([{ first_name: '1' }], 'first_name', 'ASC', 1234)).toThrow();
+    expect(() => MultiSort([{ first_name: '1' }], 'first_name', 'ASC', {foo: 'bar'})).toThrow();
+});
