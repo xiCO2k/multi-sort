f8811b154f5dba5855b885934ab44aba3ba38fba
Francisco Madeira <xico2k@gmail.com>
2021-09-18 22:36:20 +0100

    Fix indentation

--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,115 @@
 function validateData(data) {
-    if (!data || typeof data !== 'object' ||
+  if (!data || typeof data !== 'object' ||
         Array.isArray(data) && data.length &&
         (Array.isArray(data[0]) || typeof data[0] !== 'object')) {
-        throw new Error('It can\'t sort, validate the data sent');
-    }
+    throw new Error('It can\'t sort, validate the data sent');
+  }
 }
 
 function validateKey(key) {
-    if (typeof key !== 'string') {
-        throw new Error('The key param needs to be a string');
-    }
+  if (typeof key !== 'string') {
+    throw new Error('The key param needs to be a string');
+  }
 }
 
 function validateParams(data, columns, orderBy, key) {
-    validateData(data);
-    validateKey(key);
+  validateData(data);
+  validateKey(key);
 }
 
 function formatArguments(args) {
-    let columns = [],
-        orderBy = [],
-        key = '_key';
+  let columns = [];
+  let orderBy = [];
+  let key = '_key';
 
-    //If is Object
-    if (args[0] !== null &&
+  // If is Object
+  if (args[0] !== null &&
         typeof args[0] === 'object' &&
         !Array.isArray(args[0])) {
-
-        for (let column in args[0]) {
-            columns.push(column);
-            orderBy.push(args[0][column]);
-        }
-
-        key = args[1] || key;
-    } else {
-        columns = args[0];
-        orderBy = args[1];
-
-        if (typeof columns === 'string') {
-            columns = [columns];
-        }
-
-        if (typeof orderBy === 'string') {
-            orderBy = [orderBy];
-        }
-
-        key = args[2] || key;
+    for (const column in args[0]) {
+      columns.push(column);
+      orderBy.push(args[0][column]);
     }
 
-    return { columns, orderBy, key };
+    key = args[1] || key;
+  } else {
+    columns = args[0];
+    orderBy = args[1];
+
+    if (typeof columns === 'string') {
+      columns = [columns];
+    }
+
+    if (typeof orderBy === 'string') {
+      orderBy = [orderBy];
+    }
+
+    key = args[2] || key;
+  }
+
+  return { columns, orderBy, key };
 }
 
 function formatData(arr, key) {
-    if (Array.isArray(arr)) {
-        return arr;
-    }
+  if (Array.isArray(arr)) {
+    return arr;
+  }
 
-    let data = [];
+  const data = [];
 
-    for (let i in arr) {
-        data.push({ ...arr[i], [key]: i });
-    }
+  for (const i in arr) {
+    data.push({ ...arr[i], [key]: i });
+  }
 
-    return data;
+  return data;
 }
 
 function getValue(column, item) {
-    column = column.split('.');
+  column = column.split('.');
 
-    let value = item;
-    column.forEach((col) => {
-        value = value[col];
-    });
+  let value = item;
+  column.forEach((col) => {
+    value = value[col];
+  });
 
-    if (typeof value === 'boolean' ||
+  if (typeof value === 'boolean' ||
         typeof value === 'undefined') {
-        value = value ? 1 : 0;
-    }
+    value = value ? 1 : 0;
+  }
 
-    return value;
+  return value;
 }
 
 function sort(a, b, columns, orderBy, index) {
-    let direction = orderBy[index] == 'DESC' ? 1 : 0;
-    const aValue = getValue(columns[index], a);
-    const bValue = getValue(columns[index], b);
+  const direction = orderBy[index] == 'DESC' ? 1 : 0;
+  const aValue = getValue(columns[index], a);
+  const bValue = getValue(columns[index], b);
 
-    let isNumeric = !isNaN(+aValue - +bValue),
-        x = isNumeric ?
-            +aValue :
-            Array.isArray(aValue) ?
-                aValue :
-                ('' + aValue).toLowerCase(),
-        y = isNumeric ?
-            +bValue :
-            Array.isArray(bValue) ?
-                bValue :
-                ('' + bValue).toLowerCase();
+  const isNumeric = !isNaN(+aValue - +bValue);
+  const x = isNumeric ?
+    +aValue :
+    Array.isArray(aValue) ?
+      aValue :
+      (`${aValue}`).toLowerCase();
+  const y = isNumeric ?
+    +bValue :
+    Array.isArray(bValue) ?
+      bValue :
+      (`${bValue}`).toLowerCase();
 
-    if (x < y) {
-        return direction == 0 ? -1 : 1;
-    }
+  if (x < y) {
+    return direction == 0 ? -1 : 1;
+  }
 
-    if (x == y)  {
-        return columns.length - 1 > index ? sort(a, b, columns, orderBy, index+1) : 0;
-    }
+  if (x == y) {
+    return columns.length - 1 > index ? sort(a, b, columns, orderBy, index + 1) : 0;
+  }
 
-    return direction == 0 ? 1 : -1;
+  return direction == 0 ? 1 : -1;
 }
 
 export default function MultiSort(arr = [], ...args) {
-    let { columns, orderBy, key } = formatArguments(args);
-    validateParams(arr, columns, orderBy, key); //it throws
-    return formatData(arr, key).sort((a, b) => sort(a, b, columns, orderBy, 0));
+  const { columns, orderBy, key } = formatArguments(args);
+  validateParams(arr, columns, orderBy, key); // it throws
+  return formatData(arr, key).sort((a, b) => sort(a, b, columns, orderBy, 0));
 }
